686717c3cad856fc44628398bda24fc6b4f3707e
shrimanish10@gmail.com <shrimanish10@gmail.com>
2024-01-04 16:33:29 +0530

    calc

---
+++ cal1410.js
@@ -0,0 +1,16 @@
+let result = document.getElementById("result");
+
+function display(number){
+    result.value += number;
+}
+function calculate(){
+    let final_number = result.value;
+    let final_result = eval(final_number);
+    result.value = final_result;
+}
+function clrs() {
+    result.value = "";
+}
+function del() {
+    result.value = result.value.slice(0, -1);
+}
